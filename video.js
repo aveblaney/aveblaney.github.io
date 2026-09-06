c66329e32ca207f2eca3ad1d932586fa2b0d48a3
evablan <shunsuke.otsuki107@gmail.com>
2026-09-06 21:36:46 +0900

    type shi

--- video.js
+++ video.js
@@ -3,12 +3,10 @@ function playAvatarVideo() {
   
   if (!video) return;
 
-  // Reset video position, play, and fade in
   video.currentTime = 0;
   video.classList.add('playing');
   video.play().catch(err => console.log('Autoplay issue:', err));
 
-  // Fade out back to image once video ends
   video.onended = () => {
     video.classList.remove('playing');
   };
